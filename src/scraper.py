8a2546c8905fddaac9b4b2ef490c28a41029b694
Ivan Aguilar <ivanaguilarni@gmail.com>
2021-04-08 22:41:35 +0200

    write csv

--- src/scraper.py
+++ src/scraper.py
@@ -6,7 +6,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-
+import csv
 
 class CrossfitLeaderScraper():
 
@@ -36,40 +36,28 @@ class CrossfitLeaderScraper():
         bs = BeautifulSoup(html, 'html.parser')
 
         athletes = bs.findAll("tr")
-        print(athletes)
-
-		# # For each year, get its accidents' links
-		# accidents_links = []
-		# for y in years_links:
-		# 	print "Found link to a year of crash: " + self.url + y
-		# 	html = self.__download_html(self.url + y)
-		# 	current_year_accidents = self.__get_accidents_links(html)
-		# 	accidents_links.append(current_year_accidents)
-
-		# 	# Uncomment this break in case of debug mode
-		# 	# break
-
-		# # For each accident, extract its data
-		# for i in range(len(accidents_links)):
-		# 	for j in range(len(accidents_links[i])):
-		# 		print "scraping crash data: " + self.url + \
-		# 			accidents_links[i][j]
-		# 		html = self.__download_html(self.url +
-		# 			accidents_links[i][j])
-		# 		self.__scrape_example_data(html)
-
-		# Show elapsed time
+        athletes.remove(athletes[0])
+        for a in athletes:
+            first_name = a.find("div", {"class": "first-name"}).text
+            last_name = a.find("div", {"class": "last-name"}).text
+            countrie = a.find("div", {"class": "country-flag"}).next.attrs['title']
+            total_point = a.contents[2].find("div", {"class": "cell-inner"}).text
+            result_21_1 = a.contents[3].find("span", {"class": "rank"}).text + a.contents[3].find("span", {"class": "result"}).text
+            result_21_2 = a.contents[4].find("span", {"class": "rank"}).text + a.contents[3].find("span", {"class": "result"}).text
+            result_21_3 = a.contents[5].find("span", {"class": "rank"}).text + a.contents[3].find("span", {"class": "result"}).text
+            result_21_4 = a.contents[6].find("span", {"class": "rank"}).text + a.contents[3].find("span", {"class": "result"}).text
+            
+            self.data.append([first_name, last_name, countrie, total_point, result_21_1, result_21_2, result_21_3, result_21_4])
+        return self.data
         end_time = time.time()
-		# print "\nelapsed time: " + \
-		#	str(round(((end_time - start_time) / 60), 2)) + " minutes"
+        
+        print(str(round(((end_time - start_time) / 60), 2)) + " minutos")
 
     def data2csv(self, filename):
 		# Overwrite to the specified file.
 		# Create it if it does not exist.
-        file = open("../csv/" + filename, "w+")
-
-		# Dump all the data with CSV format
-        for i in range(len(self.data)):
-            for j in range(len(self.data[i])):
-                file.write(self.data[i][j] + ";")
-            file.write("\n")
+        with open('./web-scrapping-crossfit-leaderboard/csv/' + filename, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=' ', quotechar=';', quoting=csv.QUOTE_MINIMAL)
+            
+            for i in range(len(self.data)):
+                csvwriter.writerow(self.data[i])
