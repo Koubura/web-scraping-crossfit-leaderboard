b9291d76f10302cc7806ed5d4bd492795963f550
Alexander Holler Rodriguez <aholler@uoc.edu>
2021-04-10 16:56:33 +0200

    Añadido la recogida de informacion del atleta y el gimnasio, añadido una version que ataca en la API

--- src/main.py
+++ src/main.py
@@ -1,7 +1,36 @@
 from scraper import CrossfitLeaderScraper
+import os
 
+def menu():
+    
+	print ("Selecciona una opción")
+
+	print ("\t1 - Web Scrapping con Selenium")
+
+	print ("\t2 - Web Scrapping usando API")
+
+	print ("\t9 - salir")
+
+
+scaled = 0
+division = 1
+page = 1
+menu()
 output_file = "dataset.csv"
+scraper = CrossfitLeaderScraper()
+while True:
+    opcionMenu = input("inserta un numero valor >> ")
+    if opcionMenu=="1":
+        scraper.scrape("0","1","1")
+        break
+    elif opcionMenu=="2":
+        for x in range(1,6):
+            scraper.scrapeWithJson("0",str(x),"1")
+        break
+    elif opcionMenu=="9":
+        break
+    else:
+        print ("")
+        input("No has pulsado ninguna opción correcta...\npulsa una tecla para continuar")
 
-scraper = CrossfitLeaderScraper("0","1")
-scraper.scrape()
 scraper.data2csv(output_file)
--- src/scraper.py
+++ src/scraper.py
@@ -7,38 +7,95 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import csv
+import pathlib
+import time
+import json
+
 
 class CrossfitLeaderScraper():
 
-    def __init__(self, scaled, page):
-        self.url = "https://games.crossfit.com/leaderboard/open/2021?view=0&division=1&region=0&scaled=" + scaled + "&sort=0&page=" + page
-        self.page = 1
+    def __init__(self):
         self.data = []
+        self.path = str(pathlib.Path().absolute())
+        self.numAthlete = 0
         
     def __download_html(self, url):
-        browser = webdriver.Chrome(executable_path=r'/Users/ivanaguilarnieto/Downloads/chromedriver89')
+        browser = webdriver.Chrome(executable_path=self.path+'/Drivers/chromedriver')
         browser.get(url)
         element_present = EC.presence_of_element_located((By.CLASS_NAME, 'athletes'))
-        WebDriverWait(browser, 10).until(element_present)
+        WebDriverWait(browser, 20).until(element_present)
         body = browser.execute_script("return document.body")
         source = body.get_attribute('innerHTML')   
 
         #html = response.read()
         return source
-
-    def scrape(self):
-        print("Web Scraping CrossFit Leaderboard of Opens 2021 data from " + "'" + self.url + "'...")
+    
+    def __download_html_without_javascript(self, url):
+        response = urlopen(url)
+        html = response.read()
+        return html
+    
+    def __download_json(self, url):
+        response = urlopen(url)
+        _json = response.read()
+        return json.loads(_json)
+    
+    def scrapeWithJson(self,scaled, page, division):
 		# Start timer
         start_time = time.time()
+        urlJson = "https://c3po.crossfit.com/api/competitions/v2/competitions/open/2021/leaderboards?view=0&division="+ division +"&region=0&scaled=" + scaled + "&sort=0&page=" + page
+        print("Web Scraping CrossFit Leaderboard of Opens 2021 data from " + "'" + urlJson + "'...")
 
 		# Download HTML
-        html = self.__download_html(self.url)
+        _json = self.__download_json(urlJson)
+        
+        
+        for a in _json['leaderboardRows']:
+            athlete_info = {}
+            affiliate_info = {}
+            rank = a['overallRank']
+            competitor_id = a['entrant']['competitorId']
+            first_name = a['entrant']['firstName']
+            last_name = a['entrant']['lastName']
+            countrie = a['entrant']['countryOfOriginName']
+            total_point = a['overallScore']
+            result_21_1 = a['scores'][0]['rank'] + ' (' + a['scores'][0]['scoreDisplay']+')'
+            result_21_2 = a['scores'][1]['rank'] + ' (' + a['scores'][1]['scoreDisplay']+')'
+            result_21_3 = a['scores'][2]['rank'] + ' (' + a['scores'][2]['scoreDisplay']+')'
+            result_21_4 = a['scores'][3]['rank'] + ' (' + a['scores'][3]['scoreDisplay']+')'
+            athlete_info=self.getAthleteInfo(competitor_id)
+            if (athlete_info['affiliate_code']!=''):
+                affiliate_info= self.getAffiliateInfo(athlete_info['affiliate_code'])   
+            self.data.append([competitor_id,rank, first_name, last_name, countrie, total_point, result_21_1, result_21_2, result_21_3, result_21_4])
+            for x in athlete_info:
+                self.data[self.numAthlete].append(athlete_info[x])
+            for x in affiliate_info:
+                self.data[self.numAthlete].append(affiliate_info[x])
+            self.numAthlete+=1
+            time.sleep(0.5)
+            #self.data.append()
+        end_time = time.time()
+        print(str(round(((end_time - start_time) / 60), 2)) + " minutos")
+        return self.data
+        
+        
+    def scrape(self,scaled, page, division):
+		# Start timer
+        start_time = time.time()
+        url = "https://games.crossfit.com/leaderboard/open/2021?view=0&division="+ division +"&region=0&scaled=" + scaled + "&sort=0&page=" + page
+        print("Web Scraping CrossFit Leaderboard of Opens 2021 data from " + "'" + url + "'...")
+
+		# Download HTML
+        html = self.__download_html(url)
         bs = BeautifulSoup(html, 'html.parser')
 
         athletes = bs.findAll("tr")
         athletes.remove(athletes[0])
         for a in athletes:
+            athlete_info = {}
+            affiliate_info = {}
             rank = a.contents[0].find("div", {"class": "cell-inner"}).text
+            competitor_id = a.find("a",{"class": "profile-link"}).attrs['href'].replace('/athlete/','')
             first_name = a.find("div", {"class": "first-name"}).text
             last_name = a.find("div", {"class": "last-name"}).text
             countrie = a.find("div", {"class": "country-flag"}).next.attrs['title']
@@ -47,18 +104,57 @@ class CrossfitLeaderScraper():
             result_21_2 = a.contents[4].find("span", {"class": "rank"}).text + a.contents[3].find("span", {"class": "result"}).text
             result_21_3 = a.contents[5].find("span", {"class": "rank"}).text + a.contents[3].find("span", {"class": "result"}).text
             result_21_4 = a.contents[6].find("span", {"class": "rank"}).text + a.contents[3].find("span", {"class": "result"}).text
-            
-            self.data.append([rank, first_name, last_name, countrie, total_point, result_21_1, result_21_2, result_21_3, result_21_4])
-        return self.data
+            athlete_info=self.getAthleteInfo(competitor_id)
+            if (athlete_info['affiliate_code']!=''):
+                affiliate_info= self.getAffiliateInfo(athlete_info['affiliate_code'])   
+            self.data.append([competitor_id,rank, first_name, last_name, countrie, total_point, result_21_1, result_21_2, result_21_3, result_21_4])
+            for x in athlete_info:
+                self.data[self.numAthlete].append(athlete_info[x])
+            for x in affiliate_info:
+                self.data[self.numAthlete].append(affiliate_info[x])
+            self.numAthlete+=1
+            time.sleep(0.5)
+            #self.data.append()
         end_time = time.time()
-        
         print(str(round(((end_time - start_time) / 60), 2)) + " minutos")
+        return self.data
+
+    def getAthleteInfo(self,competitor_id):
+        html_athlete = self.__download_html_without_javascript("https://games.crossfit.com/athlete/"+competitor_id)
+        bsAthlete = BeautifulSoup(html_athlete, 'html.parser')
+        athlete_info={}
+        info_bar = bsAthlete.find("ul",{"class": "infobar"})
+        if (info_bar is not None):
+            #athlete_info['athlete_country']=info_bar.find_all("li")[0:1][0].find("div",{"class": "text"}).text.replace("\n","").replace(" ","")
+            info_bar_list = info_bar.find_all("li")[2:]
+            for x in info_bar_list:
+                field=x.find("div",{"class": "item-label"}).text.replace("\n","").replace(" ","").lower()
+                athlete_info[field]=x.find("div",{"class": "text"}).text.replace("\n","").replace(" ","")
+            if (athlete_info['affiliate']!='--') :
+                athlete_info['affiliate_code'] = gym_code= bsAthlete.find("ul",{"class": "infobar"}).find_all("li")[6].find("a").attrs['href'].replace('/affiliate/','')
+            else :
+                athlete_info['affiliate_code'] = ''
+        return athlete_info;
+    
+    def getAffiliateInfo(self,affiliate_id):
+        html_affiliate = self.__download_html_without_javascript("https://games.crossfit.com/affiliate/"+affiliate_id)
+        bsAffiliate = BeautifulSoup(html_affiliate, 'html.parser')
+        affiliate_info={}
+        info_bar = bsAffiliate.find("ul",{"class": "infobar"})
+        if (info_bar is not None):
+            affiliate_info['affiliate_country'] = info_bar.find_all("li")[0:1][0].find("div",{"class": "text"}).text.replace("\n","")
+            info_bar_list = info_bar.find_all("li")[1:3]
+            for x in info_bar_list:
+                field=x.find("div",{"class": "item-label"}).text.replace("\n","").replace(" ","").lower()
+                affiliate_info[field]=' '.join(x.find("div",{"class": "text"}).text.replace("\n","").split())
+        return affiliate_info
 
     def data2csv(self, filename):
 		# Overwrite to the specified file.
 		# Create it if it does not exist.
-        with open('./web-scrapping-crossfit-leaderboard/csv/' + filename, 'w', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile, delimiter=',', quotechar=';', quoting=csv.QUOTE_MINIMAL)
-            csvwriter.writerow(["Rank", "Name", "Lastname", "Country", "Points", "21.1", "21.2", "21.3", "21.4"])
+        pathDirectoryAbsolute = str(pathlib.Path().absolute())
+        with open(pathDirectoryAbsolute + '/csv/' + filename, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=',', )
+            csvwriter.writerow(["CompetitorId","Rank", "Name", "Lastname", "Country", "Points", "21.1", "21.2", "21.3", "21.4","Division","Age","Height","Weight","Affiliate","Affiliate_code","Affiliate_country","Localitation","Phone"])
             for i in range(len(self.data)):
                 csvwriter.writerow(self.data[i])
